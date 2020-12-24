cdd6fbb5dc4c2346fd62bcf3f825b494a79ea144
mustafahmadov <mustafaia@code.edu.az>
2020-12-25 00:17:09 +0400

    Blog,Blog Detail and Author models added

---
+++ EduHomeProject/Models/BlogDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHomeProject.Models
+{
+    public class BlogDetail
+    {
+        public int Id { get; set; }
+        [Required]
+        public string DetailedImage { get;set;}
+        public string FirstContent { get; set; }
+        public string SecondContent { get; set; }
+        public string ThirdContent { get; set; }
+        public string FourthContent { get; set; }
+        public bool HasDeletedTime { get; set; }
+
+        public DateTime? DeletedTime { get; set; } 
+    }
+}
