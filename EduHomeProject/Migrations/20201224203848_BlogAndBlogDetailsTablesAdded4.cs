b02b63162ec0a83c7b4c94570279b41b6e1c9eab
mustafahmadov <mustafaia@code.edu.az>
2020-12-25 11:19:46 +0400

    Blog,Blog Detail and Author tables add

---
+++ EduHomeProject/Migrations/20201224203848_BlogAndBlogDetailsTablesAdded4.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EduHomeProject.Migrations
+{
+    public partial class BlogAndBlogDetailsTablesAdded4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AuthorBlogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogId = table.Column<int>(nullable: false),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthorBlogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AuthorBlogs_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AuthorBlogs_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBlogs_AuthorId",
+                table: "AuthorBlogs",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthorBlogs_BlogId",
+                table: "AuthorBlogs",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AuthorBlogs");
+        }
+    }
+}
