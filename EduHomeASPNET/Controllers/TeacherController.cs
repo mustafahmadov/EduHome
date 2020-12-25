0e9dd86738abc5b9d55b1e0b37b496bb46b29a9a
mustafahmadov <mustafaia@code.edu.az>
2020-12-25 17:56:50 +0400

    TeacherPageFinished

--- EduHomeASPNET/Controllers/TeacherController.cs
+++ EduHomeASPNET/Controllers/TeacherController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EduHomeASPNET.DAL;
+using EduHomeASPNET.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EduHomeASPNET.Controllers
 {
@@ -18,6 +20,14 @@ namespace EduHomeASPNET.Controllers
         {
             TempData["controllerName"] = this.ControllerContext.RouteData.Values["controller"].ToString();
             return View();
+        }
+        public IActionResult TeacherDetail(int? id)
+        {
+            TempData["controllerName"] = this.ControllerContext.RouteData.Values["controller"].ToString();
+            if (id == null) return NotFound();
+            Teacher teacher = _context.Teachers.Include(t => t.TeacherDetail).Include(t => t.Skill)
+                     .Where(t => t.HasDeleted == false && t.Id==id).FirstOrDefault();
+            return View(teacher);
         }
     }
 }
