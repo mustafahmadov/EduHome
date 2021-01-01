4ec2d10bc0a5abff93e4509ab1d87ae83eb5a7fa
mustafahmadov <mustafaia@code.edu.az>
2021-01-01 18:52:15 +0400

    VideoLink crud finished

---
+++ EduHomeASPNET/Areas/Administrator/Controllers/VideosController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduHomeASPNET.DAL;
+using EduHomeASPNET.Models;
+
+namespace EduHomeASPNET.Areas.Administrator.Controllers
+{
+    [Area("Administrator")]
+    public class VideosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public VideosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Administrator/Videos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Videos.ToListAsync());
+        }
+
+        // GET: Administrator/Videos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // GET: Administrator/Videos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Administrator/Videos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,VideoLink")] Video video)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(video);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        // GET: Administrator/Videos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos.FindAsync(id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+            return View(video);
+        }
+
+        // POST: Administrator/Videos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,VideoLink")] Video video)
+        {
+            if (id != video.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(video);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VideoExists(video.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(video);
+        }
+
+        // GET: Administrator/Videos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var video = await _context.Videos
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (video == null)
+            {
+                return NotFound();
+            }
+
+            return View(video);
+        }
+
+        // POST: Administrator/Videos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var video = await _context.Videos.FindAsync(id);
+            _context.Videos.Remove(video);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VideoExists(int id)
+        {
+            return _context.Videos.Any(e => e.Id == id);
+        }
+    }
+}
