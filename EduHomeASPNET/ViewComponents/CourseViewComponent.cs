7369230526a6a78eb9d7a5e2daf9e306f3dd101c
mustafahmadov <mustafaia@code.edu.az>
2021-01-03 01:01:24 +0400

    Course view component added

---
+++ EduHomeASPNET/ViewComponents/CourseViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using EduHomeASPNET.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHomeASPNET.ViewComponents
+{
+    public class CourseViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+        public CourseViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int? page,int take = 3)
+        {
+            ViewBag.PageCount = Decimal.Ceiling((decimal)_context.Courses
+                                          .Where(b => b.HasDeleted == false).Count() / (int)take);
+            ViewBag.Page = page;
+            if (page == null)
+            {
+                return View(await _context.Courses.Include(teacher => teacher.CourseDetail).Where(b => b.HasDeleted == false)
+                                      .OrderByDescending(b => b.Id).Take((int)take).ToListAsync());
+            }
+            return View(await Task.FromResult(await _context.Courses.Include(teacher => teacher.CourseDetail).Where(b => b.HasDeleted == false)
+                                       .OrderByDescending(b => b.Id).Skip(((int)page - 1) * (int)take).Take((int)take).ToListAsync()));
+        }
+    }
+}
